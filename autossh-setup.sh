c4f3e9395a03cd5c24d3af3b25c666b220fbd064
OlJohnny <Jonatwebhan@gmail.com>
2019-04-03 22:32:06 +0200

    sudo-er check

--- autossh-setup.sh
+++ autossh-setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+##### check for root privilges #####
+if [ "$EUID" -ne 0 ]
+then
+  echo -e "\e[91mPlease run as root.\e[39m Root privileges are needed to execute ssh commands."
+  exit
+fi
+
 ##### GLOBAL VARIABLES #####
 varxserver="<SERVER NAME>"
 varxuser="<USER NAME>"
