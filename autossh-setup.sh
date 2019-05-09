023a349eebf5326c5d90c7449101f8af9eb4e3b0
OlJohnny <Jonatwebhan@gmail.com>
2019-05-09 12:01:06 +0200

    implemented best practices

--- autossh-setup.sh
+++ autossh-setup.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# github.com/OlJohnny | 2019
 
-##### check for root privilges #####
-if [ "$EUID" -ne 0 ]
-then
-  echo -e "\e[91mPlease run as root.\e[39m Root privileges are needed to execute ssh commands."
-  exit
-fi
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace		# uncomment the previous statement for debugging
+
+
 
 ##### GLOBAL VARIABLES #####
 varxserver="<SERVER NAME>"
@@ -15,117 +16,146 @@ varxport="<SSH PORT>"
 
 
 ##### FUNCTIONS #####
-
-var1func(){
-read -p $'\e[96mClear current known hosts and Key-Pairs? (y|n): \e[0m' var1
-if [[ $var1 == "y" ]]
+### loop question: clear known hosts ###
+_var1func(){
+read --prompt $'\e[96mClear current known hosts and Key-Pairs? (y|n): \e[0m' var1
+if [[ "${var1}" == "y" ]]
 then
 	echo -e "\e[92mClearing current known hosts and Key-Pairs...\e[0m"
 	rm -rf /root/.ssh
-elif [[ $var1 == "n" ]]
+elif [[ "${var1}" == "n" ]]
 then
 	echo -e "\e[91mNot clearing current known hosts and Key-Pairs\e[0m"
 else
-	var1func
+	_var1func
 fi
 }
 
 
-var2func(){
-read -p $'\e[96mGenerate & Copy Key-Pair to a server? (y|n): \e[0m' var2
-
-if [[ $var2 == "y" ]]
+### loop question: key-pair generation and copying ###
+_var2func(){
+read --prompt $'\e[96mGenerate & Copy Key-Pair to a server? (y|n): \e[0m' var2
+if [[ "${var2}" == "y" ]]
 then
 	echo -e "\e[92mGenerating new Key-Pair (Hit Enter for default values, recommended)...\e[0m"
-	ssh-keygen	-f /root/.ssh/autossh_id_rsa
-	read -p $'\e[96mEnter the Domain/IP of the server: \e[0m' var2server
-	read -p $'\e[96mEnter the Username to the server: \e[0m' var2user
-	read -p $'\e[96mEnter the SSH port to the server: \e[0m' var2port
+	ssh-keygen -f /root/.ssh/autossh_id_rsa
+	read --prompt $'\e[96mEnter the Domain/IP of the server: \e[0m' var2server
+	read --prompt $'\e[96mEnter the Username to the server: \e[0m' var2user
+	read --prompt $'\e[96mEnter the SSH port to the server: \e[0m' var2port
 	echo -e "\e[92mCopying Key-Pair to a server...\e[0m"
-	ssh-copy-id -i /root/.ssh/autossh_id_rsa -p $var2port $var2user@$var2server
+	ssh-copy-id -i /root/.ssh/autossh_id_rsa -p "${var2port}" "${var2user}"@"${var2server}"
 	echo -e "\e[92mAdding Server to known_hosts...\e[0m"
-	ssh-keyscan -H $var2server >> ~/.ssh/known_hosts
-elif [[ $var2 == "n" ]]
+	ssh-keyscan -H "${var2server}" >> ~/.ssh/known_hosts
+elif [[ "${var2}" == "n" ]]
 then
-	echo -e "\e[91mNot Copying Key-Pair to a server\e[0m"
-	echo ""
-	read -p $'\e[96mEnter the Domain/IP of the server to connect to: \e[0m' var2server
-	read -p $'\e[96mEnter the Username to the server to connect to: \e[0m' var2user
-	read -p $'\e[96mEnter the SSH port to the server: \e[0m' var2port
+	echo -e "\e[91mNot Copying Key-Pair to a server\e[0m\n"
+	read --prompt $'\e[96mEnter the Domain/IP of the server to connect to: \e[0m' var2server
+	read --prompt $'\e[96mEnter the Username to the server to connect to: \e[0m' var2user
+	read --prompt $'\e[96mEnter the SSH port to the server: \e[0m' var2port
 else
-	var2func
+	_var2func
 fi
-varxserver=$var2server
-varxuser=$var2user
-varxport=$var2port
+varxserver="${var2server}"
+varxuser="${var2user}"
+varxport="${var2port}"
 }
 
 
-var4func(){
-read -p $'\e[96mEnable script at system startup? (y|n): \e[0m' var4
-if [[ $var4 == "y" ]]
-then
-	echo -e "\e[92mEnabling script at system startup...\e[0m"
-	systemctl enable autossh-$var3name.service
-elif [[ $var4 == "n" ]]
-then
-	echo -e "\e[91mNot Enabling script at system startup\e[0m"
-else
-	var4func
-fi
-}
-
-
-
-##### ACTUAL CODE #####
-
-### Clear current known hosts and Key-Pairs ###
-var1func
-
-
-echo ""
-### Generate & Copy Key-Pair to a server ###
-var2func
-
-
-echo ""
-### Install autossh ###
-echo "Checking if autossh is installed..."
-
-if [ $(dpkg-query -W -f='${Status}' autossh 2>/dev/null | grep -c "ok installed") -eq 0 ];
+### install autossh function ###
+_var3func(){
+read --prompt $'\e[96mDo you want to install autossh? (Errors can occur, when files are in the wrong format) (y|n): \e[0m' var1
+if [[ "${var1}" -eq "y" ]]
 then
 	echo -e "\e[92mInstalling autossh...\e[0m"
 	apt-get --yes install autossh
+elif [[ "${var1}" -eq "n" ]]
+then
+	echo -e "\e[91mPackage is needed to complete the run of this script.\e[0m"
+	echo "Exiting..."
+	exit
+else
+	_var3func
+fi
+}
+
+
+### loop question: enable at system startup ###
+_var4func(){
+read --prompt $'\e[96mEnable script at system startup? (y|n): \e[0m' var4
+if [[ "${var4}" == "y" ]]
+then
+	echo -e "\e[92mEnabling script at system startup...\e[0m"
+	systemctl enable autossh-"${var3name}".service
+elif [[ "${var4}" == "n" ]]
+then
+	echo -e "\e[91mNot Enabling script at system startup\e[0m"
+else
+	_var4func
+fi
+}
+
+
+
+##### PREPARATION #####
+### check for root privilges ###
+if [[ "${EUID}" -ne 0 ]]
+then
+  echo -e "\e[91mPlease run as root.\e[39m Root privileges are needed to move and delete files"
+  exit
+fi
+
+
+### install autossh ###
+echo "Checking if autossh is installed..."
+
+if [[ $(dpkg-query --show --showformat='${Status}' autossh 2>/dev/null | grep --count "ok installed") -eq 0 ]];
+then
+	_var3func
 else
 	echo -e "\e[92mPackage autossh is already installed\e[0m"
 fi
 
 
+
+##### ACTUAL CODE #####
+### Clear current known hosts and Key-Pairs ###
 echo ""
+_var1func
+
+
+### Generate & Copy Key-Pair to a server ###
+echo ""
+_var2func
+
+
 ### Finishing touches ###
-read -p $'\e[96mCustom ssh command to be used with autossh, if needed (e.g. "-R 443:localhost:80") : \e[0m' var3custom
-read -p $'\e[96mName of the created script (e.g. Input "443-80" results in "autossh-443-80.service"): \e[0m' var3name
-echo -e "\e[92mSetting up scipt in /etc/systemd/system/autossh-"$var3name".service...\e[0m"
+read --prompt $'\n\e[96mCustom ssh command to be used with autossh, if needed (e.g. "-R 443:localhost:80") : \e[0m' var3custom
+read --prompt $'\e[96mName of the created script (e.g. Input "443-80" results in "autossh-443-80.service"): \e[0m' var3name
+echo -e "\e[92mSetting up scipt in /etc/systemd/system/autossh-"${var3name}".service...\e[0m"
 echo "[Unit]
-Description=Opens SSH Tunnel to "$varxserver"
+Description=Opens SSH Tunnel to "${varxserver}"
 After=network.target
 
 [Service]
 Environment=\"AUTOSSH_GATETIME=0\"
-ExecStart=/usr/bin/autossh -M 0 -o \"ServerAliveInterval 30\" -o \"ServerAliveCountMax 3\" -N "$var3custom" "$varxuser"@"$varxserver" -p "$varxport" -i /root/.ssh/autossh_id_rsa
+ExecStart=/usr/bin/autossh -M 0 -o \"ServerAliveInterval 30\" -o \"ServerAliveCountMax 3\" -N "${var3custom}" "${varxuser}"@"${varxserver}" -p "${varxport}" -i /root/.ssh/autossh_id_rsa
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/autossh-$var3name.service
+WantedBy=multi-user.target" > /etc/systemd/system/autossh-"${var3name}".service
 
 
-echo -e ""
 ### Starting script ###
-echo -e "\e[92mStarting script...\e[0m"
+echo -e "\n\e[92mStarting script...\e[0m"
 systemctl daemon-reload
-service autossh-$var3name start
-service autossh-$var3name status
+service autossh-"${var3name}" start
+service autossh-"${var3name}" status
 
 
-echo ""
 ### Enable script at startup ###
-var4func
+echo ""
+_var4func
+
+
+
+##### FINISHING #####
+echo -e "\n<$(date +"%T")> Finished\nExiting..."
